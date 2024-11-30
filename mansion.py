0f2e8839d0e97b62da2592ec560f83fc62fdfb4b
Ramya-Ravisankar <rb756@njit.edu>
2024-11-30 02:47:55 -0500

    changes to mansion.py

--- mansion.py
+++ mansion.py
@@ -1,31 +1,14 @@
 # mansion.py
 
-class Room:
-    def __init__(self, name):
-        self.name = name
-        self.connections = []
+class Mansion:
+    def __init__(self):
+        self.rooms = {
+            "Kitchen": ["Dining Room", "Ballroom"],
+            "Dining Room": ["Kitchen", "Library"],
+            "Ballroom": ["Kitchen", "Conservatory"],
+            "Library": ["Dining Room", "Conservatory"],
+            "Conservatory": ["Ballroom", "Library"]
+        }
 
-    def connect(self, other_room):
-        self.connections.append(other_room)
-        other_room.connections.append(self)
-
-    def __repr__(self):
-        connections = ', '.join(room.name for room in self.connections)
-        return f"{self.name} (Connected to: {connections})"
-
-def create_mansion():
-    # Define rooms
-    kitchen = Room("Kitchen")
-    library = Room("Library")
-    ballroom = Room("Ballroom")
-    study = Room("Study")
-    lounge = Room("Lounge")
-
-    # Connect rooms
-    kitchen.connect(library)
-    kitchen.connect(ballroom)
-    library.connect(study)
-    ballroom.connect(lounge)
-
-    # Return all rooms as a list
-    return [kitchen, library, ballroom, study, lounge]
+    def get_neighbors(self, room):
+        return self.rooms.get(room, [])
